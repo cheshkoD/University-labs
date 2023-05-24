252a99200cba0a53dfe698f9d36a25f19949f620
Dmitry <46524324+cheshkoD@users.noreply.github.com>
2023-05-24 22:32:20 +0300

    Delete Practice_1 directory

--- Practice_1/task_2.py
+++
@@ -1,3 +0,0 @@
-print('Hello world')
-print('My name is Dmitry, i am 17')
-print('Its my first practice in PyCharm')
--- Practice_1/task_3.py
+++
@@ -1,8 +0,0 @@
-print('This is the scrips fot calculate area of the rectangle')
-print('Please write value')
-x=int(input('Length: '))
-y=int(input('Weidth: '))
-
-area = x * y;
-
-print('The area = ', area)
